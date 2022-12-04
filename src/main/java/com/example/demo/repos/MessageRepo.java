951ee5a37ee074924da5fac8ed22d0433f60f250
Oksana <oksana@mail.ru>
2022-12-04 23:42:47 +0300

    added pagination

--- src/main/java/com/example/demo/repos/MessageRepo.java
+++ src/main/java/com/example/demo/repos/MessageRepo.java
@@ -1,10 +1,12 @@
 package com.example.demo.repos;
 
 import com.example.demo.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface MessageRepo extends CrudRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
+
+    Page<Message> findAll(Pageable pageable);
 }
